46fc1afd704ea1e6888bd2ae01a8e95a4b7c914f
Niti Mangwani <nitimangwani@Nitis-MacBook-Pro.local>
2024-04-23 22:10:44 -0400

    changed values secret

--- functions/data.js
+++ functions/data.js
@@ -6,14 +6,14 @@ exports.handler = async (event, context) => {
   };
 
     const data = {
-      X_MESSAGE: 'SGF2ZSBhIGdyZWF0IGRheQ==', // Base64 encoded string: "Have a great day"
-      Y_MESSAGE: 'bXVkaGmNdHNANHBt', // Base64 encoded string: "mudhuts@4pm"
-      VAR1: 'mudhuts',
-      VAR2: 'mudhuts@4pm',
-      LINK1: 'https://example.com/level1',
-      LINK2: 'https://example.com/level2',
-      SECRET_WORD: 'supersecret',
-      UNLOCK_CODE: 'unlockme'
+      X_MESSAGE: '42.389594', 
+      Y_MESSAGE: '-72.526612', 
+      VAR1: 'campuspond',
+      VAR2: 'campus pond',
+      LINK1: 'https://www.geeksforgeeks.org/print-longest-common-substring/',
+      LINK2: 'https://bit.ly/lastVaultFlag',
+      SECRET_WORD: 'wipuspocus',
+      UNLOCK_CODE: 'puspo'
     };
   
     return {
